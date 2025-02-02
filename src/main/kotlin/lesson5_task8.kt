90fc28cf3b24774750ee524152db390586756bdf
wildmeats <wildmeat1@yandex.ru>
2025-02-02 22:38:45 +0300

    KS-5-8 Jsup parser

---
+++ src/main/kotlin/lesson5_task8.kt
@@ -0,0 +1,19 @@
+package org.example
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+
+fun main() {
+    val doc: Document =
+        Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/")
+            .get()
+    println(doc.title())
+
+    val quotes: Elements = doc.select(".sc-2aegk7-2.bzpNIu")
+
+    for (quote in quotes) {
+        println("\n${quote.text()}\n")
+
+    }
+}
